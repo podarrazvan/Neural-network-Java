78ee2c0339e217ffdb7797a45d480ca0c77b1de0
Podar Razvan <49252921+podarrazvan@users.noreply.github.com>
2020-06-11 22:59:04 +0300

    Update Forward.java

--- Forward.java
+++ Forward.java
@@ -4,10 +4,6 @@ public class Forward {
     public double[][] Forward (double[][] inputs, int n_neurons,int l_inputs, double[][]weights) {
         double[][] output = new double[l_inputs][n_neurons];
         DotProduct dp = new DotProduct();
-//        System.out.println("Inputs");
-//        System.out.println(Arrays.deepToString(inputs));
-//      System.out.println("Weights");
-//      System.out.println(Arrays.deepToString(weights));
         output = dp.dot_product(inputs, weights);
     return output;
     }
